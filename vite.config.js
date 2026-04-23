e8fc510bf87b5930c594fcb8c261cfcde7ceebef
jurisxarajs <jurisxarajs@gmail.com>
2026-04-23 22:37:14 +0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      manifest: {
+        id: "/",
+        name: "Runology",
+        short_name: "Runology",
+        description: "Running journal app",
+        theme_color: "#111111",
+        background_color: "#111111",
+        display: "standalone",
+        start_url: "/",
+        scope: "/",
+        icons: [
+          {
+            src: "/pwa-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
